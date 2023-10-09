3e466c3cc24d4d1d2055582c3674fc36d902ee8b
Jefit Lopes <jefit.lopes@sap.com>
2023-10-09 12:53:36 -0300

    feat: Step4 done

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/m/Text"
+    "sap/ui/core/mvc/XMLView"
 
-], function(Text){
+], function(XMLView){
     "use strict";
 
-    new Text({
-        text: "Hello UI5!"
-    }).placeAt("content")
+    XMLView.create({
+        viewName: "sap.ui.demo.walkthrough.view.App"
+    }).then(function(oView){
+        oView.placeAt("content");
+    })
 });
