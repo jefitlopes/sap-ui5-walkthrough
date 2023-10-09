b6fb6af233731ba3d0e3451374eed34ad6f945fb
Jefit Lopes <jefit.lopes@sap.com>
2023-10-09 13:36:42 -0300

    feat: Step 7 done

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/m/MessageToast"
-], function(Controller, MessageToast){
+    "sap/ui/core/mvc/Controller", "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel){
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
+        onInit:function(){
+            var oData = {
+                recipient : {
+                    name : "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello: function(){
             MessageToast.show("Hello there!");
         }
