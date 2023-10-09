0de307c4c6b28f31528247214bd63a45d7a08b6b
Jefit Lopes <jefit.lopes@sap.com>
2023-10-09 13:22:12 -0300

    feat: Step6 done

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller){
+    "sap/ui/core/mvc/Controller", "sap/m/MessageToast"
+], function(Controller, MessageToast){
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello: function(){
-            alert("Hello!");
+            MessageToast.show("Hello there!");
         }
     });
 });
