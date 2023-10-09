e4f52014fc4707cb7e4297f5efaec974de63afa5
Jefit Lopes <jefit.lopes@sap.com>
2023-10-09 16:01:03 -0300

    feat: Step8 done

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller", "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function(Controller, MessageToast, JSONModel){
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function(Controller, MessageToast, JSONModel, ResourceModel){
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onInit:function(){
@@ -12,10 +13,20 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n", 
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n")
         },
 
         onShowHello: function(){
-            MessageToast.show("Hello there!");
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            MessageToast.show(sMsg);
         }
     });
 });
