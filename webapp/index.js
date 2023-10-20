f8b8d866e36a5df3e080103b13e9594ea8723062
Jefit Lopes <jefit.lopes@sap.com>
2023-10-20 12:32:35 -0300

    feat: step9 done

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,14 @@
-sap.ui.define([
-    "sap/ui/core/mvc/XMLView"
+sap.ui.define(
+	["sap/ui/core/ComponentContainer"],
+	function (ComponentContainer) {
+		"use strict";
 
-], function(XMLView){
-    "use strict";
-
-    XMLView.create({
-        viewName: "sap.ui.demo.walkthrough.view.App"
-    }).then(function(oView){
-        oView.placeAt("content");
-    })
-});
+		new ComponentContainer({
+			name: "sap.ui.demo.walkthrough",
+			settiings: {
+				id: "walkthrough",
+			},
+			async: true,
+		}).placeAt("content");
+	}
+);
