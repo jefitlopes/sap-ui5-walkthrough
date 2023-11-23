28b21f7538ea55b151dc792e696b690ff1fca2e9
Jefit Lopes <jefit.lopes@sap.com>
2023-11-23 17:25:52 -0300

    step24 done

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -2,7 +2,9 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
-], function (Controller, JSONModel, formatter) {
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict"
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
@@ -12,6 +14,19 @@ sap.ui.define([
                 currency: "EUR"
             });
             this.getView().setModel(oViewModel, "view")
+        },
+
+        onFilterInvoices(oEvent){
+            const aFilter = [];
+            const sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+
+            const oList = this.byId("invoiceList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        
         }
     })
 })
