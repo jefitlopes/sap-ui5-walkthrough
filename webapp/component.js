6243b04b217faa88fc580c25022fbae4dd7ae3a0
Jefit Lopes <jefit.lopes@sap.com>
2023-11-28 17:24:49 -0300

    step31 done

--- webapp/component.js
+++ webapp/component.js
@@ -24,6 +24,8 @@ sap.ui.define(
 				this.setModel(oModel);
 
 				this._helloDialog = new HelloDialog(this.getRootControl());
+
+				this.getRouter().initialize();
 			},
 			exit: function () {
 				this._helloDialog.destroy();
--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -30,6 +30,10 @@ sap.ui.define([
             var oBinding = oList.getBinding("items");
             oBinding.filter(aFilter);
         
+        },
+        onPress: function (oEvent) {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("detail");
         }
     })
 })
