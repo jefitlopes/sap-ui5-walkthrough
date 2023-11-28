ced7ac85aa5dcc8545e15f95ff6bcd92efeb65c8
Jefit Lopes <jefit.lopes@sap.com>
2023-11-28 18:28:00 -0300

    step36 done

--- webapp/component.js
+++ webapp/component.js
@@ -1,39 +1,57 @@
-sap.ui.define(
-	[
-		"sap/ui/core/UIComponent",
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/model/resource/ResourceModel",
-		"./controller/HelloDialog"
-	],
-	function (UIComponent, JSONModel, ResourceModel, HelloDialog) {
-		"use strict";
-		return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-			metadata: {
-				manifest: "json",
-			},
-			init: function () {
-				// call the init function of the parent
-				UIComponent.prototype.init.apply(this, arguments);
-				// set data models
-				var oData = {
-					recipient: {
-						name: "UI5",
-					},
-				};
-				var oModel = new JSONModel(oData);
-				this.setModel(oModel);
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog",
+    "sap/ui/Device"
+], function (UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata: {
+            manifest: "json"
+        },
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data models
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
 
-				this._helloDialog = new HelloDialog(this.getRootControl());
+            // set device model
+            var oDeviceModel = new JSONModel(Device);
+            oDeviceModel.setDefaultBindingMode("OneWay");
+            this.setModel(oDeviceModel, "device");
 
-				this.getRouter().initialize();
-			},
-			exit: function () {
-				this._helloDialog.destroy();
-				delete this._helloDialog;
-				},
-			openHelloDialog: function () {
-				this._helloDialog.open();
-			},
-		});
-	}
-);
+            // set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+        },
+
+        getContentDensityClass : function () {
+            if (!this._sContentDensityClass) {
+                if (!Device.support.touch) {
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else {
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+            }
+            return this._sContentDensityClass;
+        },
+
+        exit: function () {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
+        } 
+    });
+});
